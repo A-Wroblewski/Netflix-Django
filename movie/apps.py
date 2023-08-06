1b36c85b0bc547fd2312f690179a624018d829e6
Pimegonho <62357109+Pimegonho@users.noreply.github.com>
2023-08-06 14:14:32 -0300

    criar superuser

--- movie/apps.py
+++ movie/apps.py
@@ -1,6 +1,22 @@
+import os
+from .models import User
 from django.apps import AppConfig
 
 
 class MovieConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'movie'
+
+    # funcão que vai rodar quando o aplicativo for todo carregado
+    def ready(self):
+        email = os.getenv('ADMIN_EMAIL')
+        password = os.getenv('ADMIN_PASSWORD')
+
+        users = User.objects.filter(email=email)
+
+        if not users:
+            User.objects.create_superuser(
+                username='admin',
+                email=email,
+                password=password,
+            )
